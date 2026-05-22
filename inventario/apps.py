fa12526589ee8397a35f46954f7c3d003c2c3a67
Manuel Puntos <alphamegc@gmail.com>
2026-05-21 23:32:01 -0400

    admin

--- inventario/apps.py
+++ inventario/apps.py
@@ -1,5 +1,19 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
 
 class InventarioConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'inventario'
+
+    def ready(self):
+        # Conectamos la señal para que se ejecute después de las migraciones
+        post_migrate.connect(create_superuser, sender=self)
+
+def create_superuser(sender, **kwargs):
+    from django.contrib.auth.models import User
+    
+    # Verifica si el usuario 'admin' ya existe para no intentar crearlo de nuevo
+    if not User.objects.filter(username='admin').exists():
+        # AQUÍ CAMBIA 'tu_contraseña' por una contraseña segura
+        User.objects.create_superuser('admin', 'alphamegc@gmail.com', 'MEGC2525')
+        print("¡Usuario administrador creado automáticamente!")
