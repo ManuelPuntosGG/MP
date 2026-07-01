78b49ac506687e94688d4127fb32adaf85ae368d
Manuel Puntos <96326450+ManuelPuntosGG@users.noreply.github.com>
2026-07-01 13:44:39 -0400

    REWORK UI Y UX

--- inventario/views.py
+++ inventario/views.py
@@ -64,8 +64,8 @@ def obtener_tasa_binance():
             if 'rates' in data and 'VES' in data['rates']:
                 tasa_bcv = float(data['rates']['VES'])
                 
-                # Ajuste del 30% para saltar de tasa oficial a tasa P2P
-                FACTOR_AJUSTE = 1.30
+                # Ajuste del 20% para saltar de tasa oficial a tasa P2P
+                FACTOR_AJUSTE = 1.20
                 precio = round(tasa_bcv * FACTOR_AJUSTE, 2)
                 
                 cache.set('tasa_usdt_ves', precio, 3600)
