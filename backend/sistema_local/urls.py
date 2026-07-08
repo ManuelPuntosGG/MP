364bac1b0bf25c4c12ee9883ee9aa8d8a4f4378e
Manuel Puntos <96326450+ManuelPuntosGG@users.noreply.github.com>
2026-07-08 16:30:07 -0400

    Quitar PWA

--- backend/sistema_local/urls.py
+++ backend/sistema_local/urls.py
@@ -2,13 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('inventario.urls')),
-    path('manifest.json', TemplateView.as_view(template_name='pwa/manifest.json', content_type='application/json'), name='manifest'),
-    path('sw.js', TemplateView.as_view(template_name='pwa/sw.js', content_type='application/javascript'), name='sw'),
 ]
 
 # CORRECCIÓN: Quitamos 'if settings.DEBUG' para que también sirva imágenes en Render
